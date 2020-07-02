f0629521349f53810c5d73b6cb4f3f44b6187d63
Sophie Mori <ksmori14@gmail.com>
2020-07-02 15:54:18 -0400

    reorganize

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p i3 i3status bash
+
+cp -r ~/.config/i3 .
+cp -r ~/.config/i3status .
+cp ~/.bashrc bash/.bashrc
+cp ~/.bash_aliases bash/.bash_aliases
+cp ~/.gitconfig .gitconfig
